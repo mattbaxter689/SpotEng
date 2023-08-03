733aad9181d6280daa8d0fcb382df5b51c5a08af
Matthew Baxter <matthewbaxter0412@hotmail.com>
2023-08-03 01:03:56 -0400

    back to somewhat working

--- spoteng/main.py
+++ spoteng/main.py
@@ -1,4 +1,5 @@
 from utils.config import get_warehouse_creds
+from utils.db import WarehouseConnection
 from dotenv import load_dotenv
 from spotify import SpotifyHelper, SpotifyConnection
 import os
@@ -6,17 +7,21 @@ import os
 load_dotenv()
 
 def run():
-    spot_conn = SpotifyConnection(client_id=os.environ["SPOTIFY_CLIENT"],
+    spot = SpotifyConnection(client_id=os.environ["SPOTIFY_CLIENT"],
                                   client_secret=os.environ["SPOTIFY_CLIENT_SECRET"])
-    print(SpotifyHelper(spot_conn).get_saved_id())
 
-    # with WarehouseConnection(get_warehouse_creds()).managed_cursor() as curr:
-    #     songs_list = list(songs)
-    #     curr.execute("""
-    #                  INSERT INTO songs.names (
-    #                         VALUES(%s, %s)
-    #                     )
-    #                  """, songs_list)
+    scope = "user-library-read user-follow-read user-top-read playlist-read-private"
+    sp_conn = spot.connect(scope = scope)
+    help = SpotifyHelper(sp_conn=sp_conn)
+    songs = help.get_saved_id()
+
+    with WarehouseConnection(get_warehouse_creds()).managed_cursor() as curr:
+        songs_list = list(songs)
+        curr.execute("""
+                     INSERT INTO songs.names (
+                            VALUES(%s, %s)
+                        )
+                     """, songs_list)
     
 
 if __name__ == "__main__":
--- spoteng/spotify.py
+++ spoteng/spotify.py
@@ -1,20 +1,21 @@
 from pandas import DataFrame
-from spotipy import Spotify
-from spotipy.oauth2 import SpotifyOAuth
 import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from spotipy import Spotify
 
 class SpotifyConnection:
     def __init__(self, client_id: str, client_secret: str):
         self.client_id = client_id
         self.client_secret = client_secret
 
-    def connect(self, scope: str) -> Spotify:
+    def connect(self, scope) -> Spotify:
         return spotipy.Spotify(auth_manager=SpotifyOAuth(
             client_id=self.client_id,
             client_secret=self.client_secret,
             redirect_uri="http://www.google.com",
-            scope=scope
+            scope = scope
         ))
+
 
 class SpotifyHelper:
     def __init__(self, sp_conn):
@@ -28,11 +29,11 @@ class SpotifyHelper:
             results = self.sp_conn.next(results)
             saved.extend(results['items'])
 
-        saved_tracks = []
+        saved_tracks = list()
         for i in range(len(saved)):
             saved_tracks.append({
-                'id': saved_tracks[i]['track']['id'],
-                'name': saved_tracks[i]['track']['name']
+                'id': saved[i]['track']['id'],
+                'name': saved[i]['track']['name']
             })
 
         songs_pd = DataFrame(saved_tracks)
