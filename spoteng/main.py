2dd7783e85d776d68776861cd4e064156d6e5b6c
Matthew Baxter <matthewbaxter0412@hotmail.com>
2023-08-02 23:10:31 -0400

    changes

--- spoteng/main.py
+++ spoteng/main.py
@@ -1,6 +1,6 @@
 from utils.config import get_warehouse_creds
 from dotenv import load_dotenv
-from spotify import SpotifyConnection, SpotifyHelper
+from spotify import SpotifyHelper
 import os
 import psycopg2
 
@@ -9,19 +9,17 @@ from utils.db import WarehouseConnection
 load_dotenv()
 
 def run():
-    spot_conn = SpotifyConnection(client_id=os.environ["SPOTIFY_CLIENT"],
+    spot_conn = SpotifyHelper(client_id=os.environ["SPOTIFY_CLIENT"],
                                   client_secret=os.environ["SPOTIFY_CLIENT_SECRET"])
-    scope = "user-library-read user-follow-read user-top-read playlist-read-private"
-    conn = spot_conn.connect(scope=scope)
-    songs = SpotifyHelper(conn).get_saved_id().astype('string')
+    print(spot_conn.get_saved_id())
 
-    with WarehouseConnection(get_warehouse_creds()).managed_cursor() as curr:
-        songs_list = list(songs)
-        curr.execute("""
-                     INSERT INTO songs.names (
-                            VALUES(%s, %s)
-                        )
-                     """, songs_list)
+    # with WarehouseConnection(get_warehouse_creds()).managed_cursor() as curr:
+    #     songs_list = list(songs)
+    #     curr.execute("""
+    #                  INSERT INTO songs.names (
+    #                         VALUES(%s, %s)
+    #                     )
+    #                  """, songs_list)
     
 
 if __name__ == "__main__":
--- spoteng/spotify.py
+++ spoteng/spotify.py
@@ -1,40 +1,58 @@
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
-from spotipy.client import Spotify
+import requests
 from pandas import DataFrame
 import numpy as np
 
-class SpotifyConnection:
-    def __init__(self, client_id: str, client_secret: str):
-        self.client_id = client_id
-        self.client_secret = client_secret
-
-    def connect(self, scope: str) -> Spotify:
-        return spotipy.Spotify(auth_manager=SpotifyOAuth(
-            client_id=self.client_id,
-            client_secret=self.client_secret,
-            redirect_uri="http://www.google.com",
-            scope=scope
-        ))
+# class SpotifyConnection:
+#     def __init__(self, client_id: str, client_secret: str):
+#         self.client_id = client_id
+#         self.client_secret = client_secret
+#
+#     def connect(self, scope: str) -> Spotify:
+#         return spotipy.Spotify(auth_manager=SpotifyOAuth(
+#             client_id=self.client_id,
+#             client_secret=self.client_secret,
+#             redirect_uri="http://www.google.com",
+#             scope=scope
+#         ))
 
 class SpotifyHelper:
-    def __init__(self, sp_conn):
-        self.sp_conn = sp_conn
+    def __init__(self, client_secret: str, client_id: str):
+        self.client_secret = client_secret
+        self.client_id = client_id
+        self.access_token = self.get_token()
+
+    def authorize(self):
+        
+
+    def get_token(self):
+        response = requests.post(url='https://accounts.spotify.com/api/token',
+                                 data={ 'grant_type': 'authorization_code' },
+                                 auth=(self.client_id, self.client_secret))
+        self.spotify_token = response.json()['access_token']
 
     def get_saved_id(self) -> DataFrame:
-        res = self.sp_conn.current_user_saved_tracks()
-        saved = res['items']
-        while res['next']:
-            res = self.sp_conn.next(res)
-            saved.extend(res['items'])
-        
-        saved_id = list()
-        saved_name = list()
+        songs=[]
+        headers = {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization": "Bearer {token}".format(token = self.spotify_token)
+        }       
+        url = "https://api.spotify.com/v1/me/tracks"
+        while True:
+            response = requests.get(url=url, headers=headers)
+            results = response.json()
+            print(results)
 
-        for i in range(len(saved)):
-            saved_id.append(saved[i]['track']['id'])
-            saved_name.append(saved[i]['track']['name'])
+            for song in results["items"]:
+                songs.append({
+                    'id': song["track"]["id"],
+                    'name': song["track"]["name"]
+                })
 
-        saved_id_pd = DataFrame(np.column_stack([saved_id, saved_name]), columns=["id","song_name"])
+            if results["next"] is not None:
+                url = results["next"]
+            else:
+                break
 
-        return saved_id_pd
+        songs_pd = DataFrame(songs)
+        return songs_pd
